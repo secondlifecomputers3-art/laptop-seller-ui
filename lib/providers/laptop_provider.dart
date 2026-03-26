dcce014871d9d0ebe7f04ebe9955680ebf00a72e
Holan717-tech <dcruzjonah717@gmail.com>
2026-03-26 13:26:20 +0530

    Final push: all files for GitHub Pages deployment

--- lib/providers/laptop_provider.dart
+++ lib/providers/laptop_provider.dart
@@ -232,7 +232,7 @@ class LaptopProvider extends ChangeNotifier {
         graphics: 'AMD R4 Onboard',
         screenSize: 14.6,
         displayType: 'HD',
-        price: 12507,
+        price: 12508,
         stock: 1,
         imageUrl: 'assets/images/Lenovo-E41-1.jpg',
         thumbnailUrl: 'assets/thumbnails/Lenovo-E41-1.jpg',
