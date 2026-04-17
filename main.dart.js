4ffd0163b607dd33e62adcf018b8be03e427d108
secondlifecomputers3-art <secondlifecomputers3-art@users.noreply.github.com>
2026-04-17 10:37:13 +0000

    deploy: 769679e92bccf3a4d73b1bf1850d3f16076c8226

--- main.dart.js
+++ main.dart.js
@@ -28367,7 +28367,7 @@ $S:2}
 A.avO.prototype={
 $1(a){var s=A.dn().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/59aa584fdf100e6c78c785d8a5b565d1de4b48ab/":s)+a},
 $S:129}
 A.Nl.prototype={
 gA(a){var s=this.a
